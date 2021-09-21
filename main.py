f3d5c2340bcd28ee13abf9bce718068e01ab83a2
LucasSchweisthal2024 <818482@mystma.org>
2021-09-21 14:31:20 -0500

    3

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 #WRITE YOUR CODE IN THIS FILE
+def topFiveMovies(a, b, c, d, e):
+    return a + b + c + d + e
+
+print(topFiveMovies("Lord of The Rings ", "Ready Player One ", "Pirates of The Carribean", " Pixel", " The Lorax"))
+
